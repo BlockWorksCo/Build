4e548d2a2b10126b2e5c0660f9a852b952f3c0b8
Steve Tickle <Steve@BlockWorks.co>
2014-03-08 23:17:30 +0000

    - Windows host building now seems to work for MSP430, STM32, Win32 and Linux platforms.

---
+++ Windows/HostEnvironmentSetup.cmd
@@ -0,0 +1,22 @@
+
+
+rem
+rem User configurables.
+rem
+set BLOCKWORKS_BUILD_HOSTTYPE=Windows
+set BLOCKWORKS_BUILD_BASE=c:\BlockWorks\Build
+set BLOCKWORKS_PLATFORM_BASE=c:\BlockWorks\Platform
+
+
+rem
+rem Derive the correct path from the user configurables.
+rem *NOTE: Make sure that host-specific paths come *before* Common paths to allow us to 
+rem reuse the Common code while still have the ability to override for Host specifics.
+rem
+set PATH=%BLOCKWORKS_BUILD_BASE%\Common;%PATH%
+set PATH=%BLOCKWORKS_BUILD_BASE%/%BLOCKWORKS_BUILD_HOSTTYPE%:%PATH%
+set VAGRANT_CWD=%BLOCKWORKS_BUILD_BASE%\Common
+set VAGRANT_VAGRANTFILE=%BLOCKWORKS_BUILD_BASE%/Common/Vagrantfile
+
+
+
