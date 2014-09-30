ed232754a2f2e13720141bc0d0da4997c5d85a0f
Steve Tickle <Steve@ZooKoo.com>
2014-09-30 23:51:51 +0100

    - Even more setup stuff.

--- Windows/StartBuildVM.cmd
+++ Windows/StartBuildVM.cmd
@@ -1,14 +1,29 @@
 
 
+VBoxManage dhcpserver remove --netname "HostInterfaceNetworking-vboxnet0"
+VBoxManage hostonlyif create
+VBoxManage hostonlyif ipconfig "vboxnet0" --ip 172.16.250.1 --netmask 255.255.255.0
+VBoxManage dhcpserver add --ifname vboxnet0 --ip 172.16.250.1 --netmask 255.255.255.0 --lowerip 172.16.250.100 --upperip 172.16.250.200
+VBoxManage dhcpserver modify --ifname vboxnet0 --enable
+
+
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "BLOCKWORKS_BASE"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "BLOCKWORKS_BASE" --hostpath "c:\\BlockWorks"
 
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "HOSTROOT_C"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "HOSTROOT_C" --hostpath "c:\\"
+
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "HOSTROOT_G"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "HOSTROOT_G" --hostpath "g:\\"
+
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "HOSTROOT_I"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "HOSTROOT_I" --hostpath "i:\\"
+
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "HOSTROOT_J"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "HOSTROOT_J" --hostpath "j:\\"
+
+VBoxManage sharedfolder remove "BlockWorksBuild" --name "HOSTROOT_R"
 VBoxManage sharedfolder add "BlockWorksBuild" --name "HOSTROOT_R" --hostpath "r:\\"
 
 VBoxManage startvm "BlockWorksBuild" --type headless
-
-VBoxManage hostonlyif ipconfig "VirtualBox Host-Only Ethernet Adapter" --ip 172.18.250.2 --netmask 255.255.255.0
 
