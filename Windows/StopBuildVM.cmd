6f00af5186e21e4f5ac46672cf68f8d84a20e44b
Steve Tickle <Steve@ZooKoo.com>
2014-10-01 20:54:48 +0100

    - Changin back to the NAT network now I can get it to work.

--- Windows/StopBuildVM.cmd
+++ Windows/StopBuildVM.cmd
@@ -1,3 +1,4 @@
 
-ssh -i /cygdrive/c/BlockWorks/Build/Common/PrivateKey %BLOCKWORKS_BUILDVM_USERHOST% "sudo shutdown -h now"
+
+ssh -p %BLOCKWORKS_BUILDVM_SSH_PORT% -i /cygdrive/c/BlockWorks/Build/Common/PrivateKey %BLOCKWORKS_BUILDVM_USERHOST% "sudo shutdown -h now"
 
